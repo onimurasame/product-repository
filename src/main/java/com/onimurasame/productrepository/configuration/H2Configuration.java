0cc1e66121855f4aeff36cbc5c0f9de0a36d2d17
rgmarkoch <rgmarkoch@nisum.com>
2018-11-29 13:18:45 -0800

    H2 now is exposed through TCP port

---
+++ src/main/java/com/onimurasame/productrepository/configuration/H2Configuration.java
@@ -0,0 +1,17 @@
+package com.onimurasame.productrepository.configuration;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Configuration {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+
+}
